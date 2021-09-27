b21d1cd2630776c7820012801f3f48606f87fb9e
Sandeep Yadav <sy68205@gmail.com>
2021-09-27 21:18:21 +0530

    add start script

--- app.js
+++ app.js
@@ -128,12 +128,8 @@ app.get("/about", function(req, res){
 
 
 
-let port = process.env.PORT;
-
-if(port == null || port == "") {
-  port = 3000;
-}
+const port = process.env.PORT || 3000;
 
 app.listen(port, function() {
-  console.log("Server started on port 3000");
+  console.log(`Server started on port ${port}`);
 });
