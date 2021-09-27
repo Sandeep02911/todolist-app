7e3bed29a73b82e7fcbf1479e311a351d58a6590
Sandeep Yadav <sy68205@gmail.com>
2021-09-27 19:13:18 +0530

    add gitignore, update ports and add node version to package.json

--- app.js
+++ app.js
@@ -128,7 +128,11 @@ app.get("/about", function(req, res){
 
 
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
+
+if(port == null || port == "") {
+  port = 3000;
+}
 
 app.listen(port, function() {
   console.log("Server started on port 3000");
